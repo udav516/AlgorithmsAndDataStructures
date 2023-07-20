dc84295c606cf7e067c3ab55a488153340d8197f
udav516 <eldar.udav516@yandex.ru>
2023-07-20 19:36:43 +0300

    task2 add addFirst, removeFirst

--- src/main/java/org/example/seminar3/Task1.java
+++ src/main/java/org/example/seminar3/Task1.java
@@ -8,4 +8,16 @@ public class Task1 {
         private int value;
     }
 
+    public void addFirst(int value) {
+        Node node = new Node();
+        node.value = value;
+        node.next = head;
+        head = node;
+    }
+
+    public void removeFirst() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
 }
