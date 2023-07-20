73f936dbf9cf0093b5ced1d10cafee682c0ff3a4
udav516 <eldar.udav516@yandex.ru>
2023-07-20 19:41:51 +0300

    task4 add addLast removeLast

--- src/main/java/org/example/seminar3/Task1.java
+++ src/main/java/org/example/seminar3/Task1.java
@@ -31,4 +31,33 @@ public class Task1 {
         }
         return false;
     }
+
+    public void addLast(int value) {
+        Node node = new Node();
+        node.value = value;
+        if (head == null) {
+            head = node;
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = node;
+        }
+    }
+
+
+    public void removeLast() {
+        if (head != null) {
+            Node node = head;
+            while (node.next != null) {
+                if (node.next.next == null) {
+                    node.next = null;
+                    return;
+                }
+                node = node.next;
+            }
+            head = null;
+        }
+    }
 }
