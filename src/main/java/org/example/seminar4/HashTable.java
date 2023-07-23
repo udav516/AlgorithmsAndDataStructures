dd628077c967902c20591fe3118df77ad91e601a
udav516 <eldar.udav516@yandex.ru>
2023-07-23 09:49:01 +0300

    add task2

--- src/main/java/org/example/seminar4/HashTable.java
+++ src/main/java/org/example/seminar4/HashTable.java
@@ -1,17 +1,28 @@
 package org.example.seminar4;
 
-public class HashTable {
+public class HashTable<K, V> {
+    private final int SIZE = 10;
+    List[] list;
+
+    public HashTable(int size) {
+        list = (List[]) new Object[size];
+    }
+
+    public int calculateBacketIndex(K key) {
+        return key.hashCode() % list.length;
+    }
+
     class Entity {
-        private int key;
-        private int value;
+        private K key;
+        private V value;
     }
 
     class List {
-        private Node node;
+        private Node head;
 
         class Node {
-            private Entity key;
-            private Node node;
+            private Entity value;
+            private Node next;
         }
     }
 
